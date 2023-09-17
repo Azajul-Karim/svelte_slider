624705a3029950c7b4ea00802c6642f5ee9bdc83
Azajul-Karim <azajulkarim2021@gmail.com>
2023-09-17 11:41:22 +0600

    first commit

---
+++ src/Image.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  const fire = createEventDispatcher();
+
+  export let slider = {};
+  export let index;
+</script>
+
+<div class="container">
+  <div
+    on:click={(event) => fire("active_slider", { index })}
+    class="panel {slider.active ? 'active' : ''}"
+    style="background-image: url('{slider.url}') "
+  >
+    <h3>{slider.name}</h3>
+  </div>
+</div>
